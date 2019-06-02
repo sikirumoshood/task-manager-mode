8b8934e557ccb60dc4bfa05a9bf0bf5ec36db4ce
sikirumoshood <sikirumoshood@gmail.com>
2019-06-02 11:55:37 +0100

    Setup webfontloader

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Container, Row, Col, Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <h3>Hello World</h3>
+        <Row>
+          <Col className="col-md-5">
+            <p>
+              Welcome to react fontawesome{" "}
+              <Button color="white" className="ml-4">
+                <FontAwesomeIcon icon={faCheck} style={{ color: "green" }} />
+              </Button>
+            </p>
+          </Col>
+
+          <Col />
+        </Row>
+      </Container>
+    );
+  }
 }
-
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import WebFont from "webfontloader";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+WebFont.load({
+  google: {
+    families: ["Kanit", "Dancing Script", "sans-serif"]
+  }
+});
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
