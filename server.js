6fa5f8a70ffd41cb2cc6391cddeb35ecaad4f6f4
sikirumoshood <sikirumoshood@gmail.com>
2019-06-08 20:49:34 +0100

    Refactor

--- server.js
+++ server.js
@@ -36,6 +36,16 @@ app.use("/api/users", users);
 app.use("/api/tasks", tasks);
 const port = process.env.PORT || 5000;
 
+//Connect server to static asset in client
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build/"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log("Server running on port " + port);
 });
