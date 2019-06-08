3becd5cfb372ab606beb3800fd3e0778ae510251
sikirumoshood <sikirumoshood@gmail.com>
2019-06-08 20:24:04 +0100

    prep for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoUri:
-    "mongodb://sikirumoshood:Olanrewaju9@ds261096.mlab.com:61096/taskmanager",
-  secretOrKey: "Olanrewaju9" //For passport
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
